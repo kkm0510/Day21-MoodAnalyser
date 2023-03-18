bbb2730c33a8e34b212871834703c3b32a0a5d0c
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-18 15:36:11 +0530

    REFACTORED : code for proper indentation

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -4,48 +4,43 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 
     @Test
-    public void givenIAmInSadMood_MessageShouldReturnSad(){
-        MoodAnalyser analyser=new MoodAnalyser("I am in sad mood");
-        try{
-            String mood= analyser.analyseMood();
+    public void givenIAmInSadMood_MessageShouldReturnSad() {
+        MoodAnalyser analyser = new MoodAnalyser("I am in sad mood");
+        try {
+            String mood = analyser.analyseMood();
             Assert.assertEquals("SAD", mood);
-        }
-        catch (MoodAnalysisException e){
+        } catch (MoodAnalysisException e) {
             System.out.println(e.getMessage());
         }
-
     }
 
     @Test
-    public void givenIAmInAnyMood_MessageShouldReturnHappy(){
-        MoodAnalyser analyser=new MoodAnalyser("I am in any mood");
-        try{
-            String mood= analyser.analyseMood();
+    public void givenIAmInAnyMood_MessageShouldReturnHappy() {
+        MoodAnalyser analyser = new MoodAnalyser("I am in any mood");
+        try {
+            String mood = analyser.analyseMood();
             Assert.assertEquals("HAPPY", mood);
-        }
-        catch (MoodAnalysisException e){
+        } catch (MoodAnalysisException e) {
             System.out.println(e.getMessage());
         }
     }
 
     @Test(expected = Exception.class)
-    public void givenNullMood_MessageShouldRThrowMoodAnalysisException(){
-        MoodAnalyser analyser=new MoodAnalyser(null);
-        try{
+    public void givenNullMood_MessageShouldRThrowMoodAnalysisException() {
+        MoodAnalyser analyser = new MoodAnalyser(null);
+        try {
             analyser.analyseMood();
-        }
-        catch (MoodAnalysisException e){
+        } catch (MoodAnalysisException e) {
             System.out.println(e.getMessage());
         }
     }
 
     @Test
-    public void givenEmptyMood_MessageShouldRThrowMoodAnalysisException(){
-        MoodAnalyser analyser=new MoodAnalyser("");
-        try{
+    public void givenEmptyMood_MessageShouldRThrowMoodAnalysisException() {
+        MoodAnalyser analyser = new MoodAnalyser("");
+        try {
             analyser.analyseMood();
-        }
-        catch (MoodAnalysisException e){
+        } catch (MoodAnalysisException e) {
             Assert.assertEquals(MoodAnalysisException.MoodExceptionType.EMPTY, e.type);
         }
     }
