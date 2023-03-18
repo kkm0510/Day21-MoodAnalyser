b2d848264e90ac1e6b013a35c1bb4c8f6bf71939
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-18 11:55:32 +0530

    REFACTORED : code to add and work with message field

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,6 +1,16 @@
 public class MoodAnalyser {
 
-    public String analyseMood(String message){
+    public String message;
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public MoodAnalyser() {
+
+    }
+
+    public String analyseMood() {
         if (message.equals("I am in sad mood"))
             return "SAD";
         return "HAPPY";
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -5,15 +5,15 @@ public class MoodAnalyserTest {
 
     @Test
     public void givenIAmInSadMood_MessageShouldReturnSad(){
-        MoodAnalyser analyser=new MoodAnalyser();
-        String mood= analyser.analyseMood("I am in sad mood");
+        MoodAnalyser analyser=new MoodAnalyser("I am in sad mood");
+        String mood= analyser.analyseMood();
         Assert.assertEquals("SAD", mood);
     }
 
     @Test
     public void givenIAmInAnyMood_MessageShouldReturnHappy(){
-        MoodAnalyser analyser=new MoodAnalyser();
-        String mood= analyser.analyseMood("I am in any mood");
+        MoodAnalyser analyser=new MoodAnalyser("I am in any mood");
+        String mood= analyser.analyseMood();
         Assert.assertEquals("HAPPY", mood);
     }
 }
