f82b51470c8bf2b7e59ecdf82adb33a680825b25
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-18 11:49:28 +0530

    ADDED : test cases for sad and any mood

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -9,4 +9,11 @@ public class MoodAnalyserTest {
         String mood= analyser.analyseMood("I am in sad mood");
         Assert.assertEquals("SAD", mood);
     }
+
+    @Test
+    public void givenIAmInAnyMood_MessageShouldReturnHappy(){
+        MoodAnalyser analyser=new MoodAnalyser();
+        String mood= analyser.analyseMood("I am in any mood");
+        Assert.assertEquals("HAPPY", mood);
+    }
 }
